f034943b21103642ce6e6f019496ced3eff5ffc3
aljazmc <mlinaric.aljaz@gmail.com>
2023-05-08 12:00:52 +0200

    feat: directory check

--- Analiza.sh
+++ Analiza.sh
@@ -2,3 +2,8 @@
 
 dirname="Analiza"
 url="https://www.dlib.si/results/?=&query=%27rele%253dAnaliza%2b(Ljubljana)%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100"
+
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
