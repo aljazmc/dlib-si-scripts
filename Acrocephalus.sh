d57c8b8d6d2e04bbe712d1a299cbe3e8301b5a4c
aljazmc <mlinaric.aljaz@gmail.com>
2023-05-22 08:37:41 +0200

    feat: add variables

---
+++ Acrocephalus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dirname="Acrocephalus"
+url="https://www.dlib.si/results/?=&query=%27rele%253dAcrocephalus%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100&page="
+
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
