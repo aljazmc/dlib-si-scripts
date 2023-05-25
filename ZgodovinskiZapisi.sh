417e7a3ba324f4364388655689df3adaaab0127b
aljazmc <mlinaric.aljaz@gmail.com>
2023-05-25 07:10:31 +0200

    feat: add variables

---
+++ ZgodovinskiZapisi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dirname="Zgodovinski Zapisi"
+url="https://www.dlib.si/results/?=&query=%27rele%253dZgodovinski%2bzapisi%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100"
