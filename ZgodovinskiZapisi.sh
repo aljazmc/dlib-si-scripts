2126fa39f5657bde7e6ce1bc5457e660da66721d
aljazmc <mlinaric.aljaz@gmail.com>
2023-05-25 08:01:29 +0200

    feat: add directory check

--- ZgodovinskiZapisi.sh
+++ ZgodovinskiZapisi.sh
@@ -2,3 +2,8 @@
 
 dirname="Zgodovinski Zapisi"
 url="https://www.dlib.si/results/?=&query=%27rele%253dZgodovinski%2bzapisi%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100"
+
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
