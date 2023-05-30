ad2381e493711446e2f1b7706518696a88b5ef33
aljazmc <mlinaric.aljaz@gmail.com>
2023-05-30 07:09:39 +0200

    feat: add variables

---
+++ ZgodovinskiCasopis.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dirname="Zgodovinski Casopis"
+url="https://www.dlib.si/results/?=&query=%27rele%253dZgodovinski%2b%25c4%258dasopis%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date&page="
