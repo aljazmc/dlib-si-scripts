b79b62145b562a595bfa6f77b80ffcd5a7029b79
aljazmc <mlinaric.aljaz@gmail.com>
2023-05-30 07:23:09 +0200

    feat: add directory check

--- ZgodovinskiCasopis.sh
+++ ZgodovinskiCasopis.sh
@@ -2,3 +2,8 @@
 
 dirname="Zgodovinski Casopis"
 url="https://www.dlib.si/results/?=&query=%27rele%253dZgodovinski%2b%25c4%258dasopis%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date&page="
+
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
