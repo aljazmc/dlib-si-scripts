896d714917881479b188a59ef7aa5a9bc1ff953b
aljazmc <mlinaric.aljaz@gmail.com>
2023-06-05 08:18:34 +0200

    feat: add directory check

--- ZgodovinaZaVse.sh
+++ ZgodovinaZaVse.sh
@@ -2,3 +2,8 @@
 
 dirname="Zgodovina za Vse"
 url="https://www.dlib.si/results/?=&query=%27rele%253dZgodovina%2bza%2bvse%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
