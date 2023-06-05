3ddbaead2c38434309955da946316274420a04ec
aljazmc <mlinaric.aljaz@gmail.com>
2023-06-05 08:14:06 +0200

    feat: add variables

---
+++ ZgodovinaZaVse.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dirname="Zgodovina za Vse"
+url="https://www.dlib.si/results/?=&query=%27rele%253dZgodovina%2bza%2bvse%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
