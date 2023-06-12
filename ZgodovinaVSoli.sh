e29b3f47947aaf801626dee4d69f865e523aed1b
aljazmc <mlinaric.aljaz@gmail.com>
2023-06-12 08:02:24 +0200

    feat: add variables

---
+++ ZgodovinaVSoli.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dirname="Zgodovina v Soli"
+url="https://www.dlib.si/results/?=&query=%27rele%253dZgodovina%2bv%2b%25c5%25a1oli%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
