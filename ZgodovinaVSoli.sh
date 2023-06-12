d6953f1ac77c3e55af0a900ab195f9f1771b60f8
aljazmc <mlinaric.aljaz@gmail.com>
2023-06-12 08:12:03 +0200

    feat: add directory check

--- ZgodovinaVSoli.sh
+++ ZgodovinaVSoli.sh
@@ -2,3 +2,8 @@
 
 dirname="Zgodovina v Soli"
 url="https://www.dlib.si/results/?=&query=%27rele%253dZgodovina%2bv%2b%25c5%25a1oli%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
