5641098be91b7f8eb3665778311cae251fd2de3e
aljazmc <mlinaric.aljaz@gmail.com>
2023-06-15 07:24:08 +0200

    feat: add directory check

--- ZdravstvenoVarstvo.sh
+++ ZdravstvenoVarstvo.sh
@@ -2,3 +2,8 @@
 
 dirname="Zdravstveno Varstvo"
 url="https://www.dlib.si/results/?=&query=%27rele%253dZdravstveno%2bvarstvo%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
