373e1bde56cc732d69981459dfe2e2cabc2e243a
aljazmc <mlinaric.aljaz@gmail.com>
2023-06-15 07:15:38 +0200

    feat: add variables

---
+++ ZdravstvenoVarstvo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dirname="Zdravstveno Varstvo"
+url="https://www.dlib.si/results/?=&query=%27rele%253dZdravstveno%2bvarstvo%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
