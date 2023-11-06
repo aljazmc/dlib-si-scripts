5a63e788aac34d4c81d988d9a839a0b63e5bd3ce
aljazmc <mlinaric.aljaz@gmail.com>
2023-11-06 07:49:09 +0200

    feat: add variables

---
+++ ZdravniskiVestnik.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dirname="Zdravniski Vestnik"
+url="https://www.dlib.si/results/?=&query=%27rele%253dZdravni%25c5%25a1ki%2bvestnik%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date&page="
