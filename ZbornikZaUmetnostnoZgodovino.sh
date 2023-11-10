d9e49693fc5589f37d1f8909b86966b06e928d5b
aljazmc <mlinaric.aljaz@gmail.com>
2023-11-10 19:25:19 +0200

    feat: add variables

---
+++ ZbornikZaUmetnostnoZgodovino.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Zbornik za Umetnostno Zgodovino"
+url="https://www.dlib.si/results/?=&query=%27rele%253dZbornik%2bza%2bumetnostno%2bzgodovino%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
