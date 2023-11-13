12750d90f6035816c6b1776d1c6674b8c34c3625
aljazmc <mlinaric.aljaz@gmail.com>
2023-11-13 08:20:39 +0200

    feat: add directory check

--- ZbornikZaUmetnostnoZgodovino.sh
+++ ZbornikZaUmetnostnoZgodovino.sh
@@ -3,3 +3,8 @@
 dirname="Zbornik za Umetnostno Zgodovino"
 url="https://www.dlib.si/results/?=&query=%27rele%253dZbornik%2bza%2bumetnostno%2bzgodovino%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
