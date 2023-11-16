f24bf5b8e0b70b3a2a5cc3ef89bae99564eb73ba
aljazmc <mlinaric.aljaz@gmail.com>
2023-11-16 08:01:48 +0200

    feat: add variables

---
+++ ActaAgriculturaeSlovenica.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dirname="Acta Agriculturae Slovenica"
+url="https://www.dlib.si/results/?=&query=%27rele%253dActa%2bagriculturae%2bSlovenica%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
