b53e369ec7f7fae2b70acf5c771ecd2cd21f4763
aljazmc <mlinaric.aljaz@gmail.com>
2023-11-21 18:30:24 +0200

    feat: add cleanup subroutine

--- ActaAgriculturaeSlovenica.sh
+++ ActaAgriculturaeSlovenica.sh
@@ -41,3 +41,4 @@ find . -type f -name "PDF*" -print0 | xargs -0I {} sh -c 'mv "{}" "{}".pdf'
 cat textstodownload | xargs wget -w 2
 find . -type f -name "TEXT*" -print0 | xargs -0I {} sh -c 'mv "{}" "{}".txt'
 
+rm index* htmldump pdflist pdfstodownload PDF.pdf textlist textstodownload TEXT.txt
