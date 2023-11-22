204c8b78a5a442a8e9039e324f08a5cf21d7d9d0
aljazmc <mlinaric.aljaz@gmail.com>
2023-11-22 08:00:54 +0200

    feat: add variables

---
+++ ActaBiologicaSlovenica.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Acta Biologica Slovenica"
+url="https://www.dlib.si/results/?=&query=%27rele%253dActa%2bbiologica%2bslovenica%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
