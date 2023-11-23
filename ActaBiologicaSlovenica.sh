24027ff1017bab6e8046a30df61fed5c919b4764
aljazmc <mlinaric.aljaz@gmail.com>
2023-11-23 09:10:54 +0200

    feat: add directory check

--- ActaBiologicaSlovenica.sh
+++ ActaBiologicaSlovenica.sh
@@ -3,3 +3,8 @@
 dirname="Acta Biologica Slovenica"
 url="https://www.dlib.si/results/?=&query=%27rele%253dActa%2bbiologica%2bslovenica%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
