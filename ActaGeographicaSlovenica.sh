8103c27bad659df7a9ea785d78e8f1ec5d01ffed
aljazmc <mlinaric.aljaz@gmail.com>
2023-11-28 08:45:16 +0200

    feat: add variables

---
+++ ActaGeographicaSlovenica.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Acta Geographica Slovenica"
+url="https://www.dlib.si/results/?=&query=%27rele%253dActa%2bgeographica%2bSlovenica%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100"
+
