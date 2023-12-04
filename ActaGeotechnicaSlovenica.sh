a1884ae654846f39b8237ffd6087716413c4a287
aljazmc <mlinaric.aljaz@gmail.com>
2023-12-04 08:16:32 +0200

    feat: add directory check

--- ActaGeotechnicaSlovenica.sh
+++ ActaGeotechnicaSlovenica.sh
@@ -3,3 +3,8 @@
 dirname="Acta Geotechnica Slovenica"
 url="https://www.dlib.si/results/?=&query=%27rele%253dActa%2bgeotechnica%2bSlovenica%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
