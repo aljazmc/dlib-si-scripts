e1370fdebcb31d1acb9203fb4a36b5d77d3ff466
aljazmc <mlinaric.aljaz@gmail.com>
2023-12-13 08:25:24 +0200

    feat: add directory check

--- CasopisZaKritikoZnanosti.sh
+++ CasopisZaKritikoZnanosti.sh
@@ -3,3 +3,8 @@
 dirname="Casopis za Kritiko Znanosti"
 url="https://www.dlib.si/results/?=&query=%27rele%253d%25c4%258casopis%2bza%2bkritiko%2bznanosti%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100&page="
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
