a894401e16d146781d17c74aaf7e2dc37275e351
aljazmc <mlinaric.aljaz@gmail.com>
2023-12-13 08:07:21 +0200

    feat: add variables

---
+++ CasopisZaKritikoZnanosti.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Casopis za Kritiko Znanosti"
+url="https://www.dlib.si/results/?=&query=%27rele%253d%25c4%258casopis%2bza%2bkritiko%2bznanosti%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100&page="
+
