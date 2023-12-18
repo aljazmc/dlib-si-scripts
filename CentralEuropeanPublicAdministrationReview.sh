9ed55ec44f9081171b3fbf890550b592eae353fe
aljazmc <mlinaric.aljaz@gmail.com>
2023-12-18 08:21:57 +0200

    feat: add directory check

--- CentralEuropeanPublicAdministrationReview.sh
+++ CentralEuropeanPublicAdministrationReview.sh
@@ -3,3 +3,8 @@
 dirname="Central European Public Administration Review"
 url="https://www.dlib.si/results/?=&query=%27rele%253dCentral%2bEuropean%2bpublic%2badministration%2breview%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
