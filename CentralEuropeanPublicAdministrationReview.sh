87253c0ddf2c3ef39f3c8bcb0cf0cade1e378e7d
aljazmc <mlinaric.aljaz@gmail.com>
2023-12-18 07:51:59 +0200

    feat: add variables

---
+++ CentralEuropeanPublicAdministrationReview.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Central European Public Administration Review"
+url="https://www.dlib.si/results/?=&query=%27rele%253dCentral%2bEuropean%2bpublic%2badministration%2breview%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
