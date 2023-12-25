9930f5152ce04c0cec03ec957afc0ca2795ac820
aljazmc <mlinaric.aljaz@gmail.com>
2023-12-25 08:31:38 +0200

    feat: add directory check

--- AndragoskaSpoznanja.sh
+++ AndragoskaSpoznanja.sh
@@ -3,4 +3,8 @@
 dirname="Andragoska Spoznanja"
 url="https://www.dlib.si/results/?=&query=%27rele%253dAS.%2bAndrago%25c5%25a1ka%2bspoznanja%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
 
