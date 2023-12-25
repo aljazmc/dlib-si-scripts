8b73ba48d4ceaf940edf93a46f8ff113f58a6293
aljazmc <mlinaric.aljaz@gmail.com>
2023-12-25 08:03:34 +0200

    feat: add variables

---
+++ AndragoskaSpoznanja.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+dirname="Andragoska Spoznanja"
+url="https://www.dlib.si/results/?=&query=%27rele%253dAS.%2bAndrago%25c5%25a1ka%2bspoznanja%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
+
