239c8c30398b969e58b58a46dd361c1c64c78a45
aljazmc <mlinaric.aljaz@gmail.com>
2024-01-01 08:41:06 +0200

    feat: add directory check

--- FilozofskiVestnik.sh
+++ FilozofskiVestnik.sh
@@ -3,3 +3,8 @@
 dirname="Filozofski Vestnik"
 url="https://www.dlib.si/results/?=&query=%27rele%253dFilozofski%2bvestnik%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
