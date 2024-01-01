bb496377187f7c82474340a1e6be181bf414b335
aljazmc <mlinaric.aljaz@gmail.com>
2024-01-01 08:32:26 +0200

    feat: add variables

---
+++ FilozofskiVestnik.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Filozofski Vestnik"
+url="https://www.dlib.si/results/?=&query=%27rele%253dFilozofski%2bvestnik%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
