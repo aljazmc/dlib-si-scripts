f87f7303a25e62f6c96fe97c22c47edf372226a3
aljazmc <mlinaric.aljaz@gmail.com>
2024-01-08 08:00:55 +0200

    feat: add directory check

--- FizikaVSoli.sh
+++ FizikaVSoli.sh
@@ -3,3 +3,8 @@
 dirname="Fizika v Soli"
 url="https://www.dlib.si/results/?query=%27rele%253dFizika%2bv%2b%25c5%25a1oli%27&pageSize=100&sortDir=ASC&sort=date&fformattypeserial=journal"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
