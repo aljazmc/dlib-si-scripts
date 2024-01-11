c1ebed30e2f1591c43d2422e3eed9fa4b1d17d3a
aljazmc <mlinaric.aljaz@gmail.com>
2024-01-11 08:49:31 +0200

    feat: add variables

---
+++ GeografijaVSoli.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Geografija v Soli"
+url="https://www.dlib.si/results/?query=%27rele%253dGeografija%2bv%2b%25c5%25a1oli%27&pageSize=100&sortDir=ASC&sort=date&fformattypeserial=journal"
+
