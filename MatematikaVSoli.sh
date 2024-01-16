89a7e044b235b83fcbe4d199c1fca6257410f146
aljazmc <mlinaric.aljaz@gmail.com>
2024-01-16 08:21:22 +0200

    feat: add variables

---
+++ MatematikaVSoli.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Matematika v Soli"
+url="https://www.dlib.si/results/?=&query=%27rele%253dMatematika%2bv%2b%25c5%25a1oli%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
