4275604b19a947df171d205dcca7985e00c38530
aljazmc <mlinaric.aljaz@gmail.com>
2024-02-01 08:31:41 +0200

    feat: add directory check

--- MednarodnaRevijaZaJavnoUpravo.sh
+++ MednarodnaRevijaZaJavnoUpravo.sh
@@ -3,3 +3,8 @@
 dirname="Mednarodna Revija za Javno Upravo"
 url="https://www.dlib.si/results/?=&query=%27rele%253dMednarodna%2brevija%2bza%2bjavno%2bupravo%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
