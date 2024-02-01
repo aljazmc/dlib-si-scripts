66a804de7d469b27a2109d035753e091697b08cd
aljazmc <mlinaric.aljaz@gmail.com>
2024-02-01 08:03:39 +0200

    feat: add variables

---
+++ MednarodnaRevijaZaJavnoUpravo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Mednarodna Revija za Javno Upravo"
+url="https://www.dlib.si/results/?=&query=%27rele%253dMednarodna%2brevija%2bza%2bjavno%2bupravo%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
