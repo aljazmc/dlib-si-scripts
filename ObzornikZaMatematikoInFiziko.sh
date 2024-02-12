6755e86d29f2c264f0db6c9c4e01f0e988723a28
aljazmc <mlinaric.aljaz@gmail.com>
2024-02-12 07:51:14 +0200

    feat: add directory check

--- ObzornikZaMatematikoInFiziko.sh
+++ ObzornikZaMatematikoInFiziko.sh
@@ -3,3 +3,8 @@
 dirname="Obzornik za Matematiko in Fiziko"
 url="https://www.dlib.si/results/?=&query=%27rele%253dObzornik%2bza%2bmatematiko%2bin%2bfiziko%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
