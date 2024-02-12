c2804d57cb561f7eba0422774c4726659ee764b5
aljazmc <mlinaric.aljaz@gmail.com>
2024-02-12 07:29:48 +0200

    feat: add variables

---
+++ ObzornikZaMatematikoInFiziko.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Obzornik za Matematiko in Fiziko"
+url="https://www.dlib.si/results/?=&query=%27rele%253dObzornik%2bza%2bmatematiko%2bin%2bfiziko%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
