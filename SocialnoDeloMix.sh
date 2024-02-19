8f7425e5aa9432987c56e1125765cb2c444c5154
aljazmc <mlinaric.aljaz@gmail.com>
2024-02-19 07:44:02 +0200

    feat: add directory check

--- SocialnoDeloMix.sh
+++ SocialnoDeloMix.sh
@@ -3,3 +3,8 @@
 dirname="Socialno Delo Mix"
 url="https://www.dlib.si/results/?=&query=%27rele%253dSocialno%2bdelo%27&pageSize=100&sortDir=ASC&sort=date&page="
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
