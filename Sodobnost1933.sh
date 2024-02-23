3a25e09f9d9c84aef709866827c2d2a8f1e05048
aljazmc <mlinaric.aljaz@gmail.com>
2024-02-23 08:50:31 +0200

    feat: add directory check

--- Sodobnost1933.sh
+++ Sodobnost1933.sh
@@ -3,3 +3,8 @@
 dirname="Sodobnost 1933"
 url="https://www.dlib.si/results/?=&query=%27rele%253dSodobnost%2b(1933)%27&pageSize=100&sortDir=ASC&sort=date&page="
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
