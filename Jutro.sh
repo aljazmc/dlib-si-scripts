1a6dc52a921017084c19c94c074aa05c343e0903
aljazmc <mlinaric.aljaz@gmail.com>
2024-03-18 07:49:13 +0200

    feat: add directory check

--- Jutro.sh
+++ Jutro.sh
@@ -3,3 +3,8 @@
 dirname="Jutro"
 url="https://www.dlib.si/results/?=&query=%27rele%253dJutro%2b(Ljubljana)%27&pageSize=100&fformattypeserial=newspaper&sortDir=ASC&sort=date&page="
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
