f95240012dd1c93a4be69974df8e3c62e9b17cc8
aljazmc <mlinaric.aljaz@gmail.com>
2024-03-27 08:09:14 +0200

    feat: add variables

---
+++ IzziviManagementu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Izzivi Managementu"
+url="https://www.dlib.si/results/?=&query=%27rele%253dIzzivi%2bmanagementu%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
