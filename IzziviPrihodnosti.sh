39cbf44759f900397b32462a2d8f5782317a85b2
aljazmc <mlinaric.aljaz@gmail.com>
2024-04-01 09:16:07 +0200

    feat: add directory check

--- IzziviPrihodnosti.sh
+++ IzziviPrihodnosti.sh
@@ -3,3 +3,8 @@
 dirname="Izzivi Prihodnosti"
 url="https://www.dlib.si/results/?=&query=%27rele%253dIzzivi%2bprihodnosti%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
