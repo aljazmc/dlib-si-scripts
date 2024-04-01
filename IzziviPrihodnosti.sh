62a4e6eba0dbbd821c0dda63486a2ceb0e76b665
aljazmc <mlinaric.aljaz@gmail.com>
2024-04-01 08:51:04 +0200

    feat: add variables

---
+++ IzziviPrihodnosti.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Izzivi Prihodnosti"
+url="https://www.dlib.si/results/?=&query=%27rele%253dIzzivi%2bprihodnosti%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100"
+
