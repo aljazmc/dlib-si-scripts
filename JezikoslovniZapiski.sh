9241ec5a2d6c3bf08a66b1333307b3ffab027b31
aljazmc <mlinaric.aljaz@gmail.com>
2024-04-08 07:04:19 +0200

    feat: add variables

---
+++ JezikoslovniZapiski.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Jezikoslovni Zapiski"
+url="https://www.dlib.si/results/?=&query=%27rele%253dJezikoslovni%2bzapiski%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100"
+
