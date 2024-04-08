c4a33a8930cba11632791bb1ad160a867e1a6fcd
aljazmc <mlinaric.aljaz@gmail.com>
2024-04-08 07:23:40 +0200

    feat: add directory check

--- JezikoslovniZapiski.sh
+++ JezikoslovniZapiski.sh
@@ -3,3 +3,8 @@
 dirname="Jezikoslovni Zapiski"
 url="https://www.dlib.si/results/?=&query=%27rele%253dJezikoslovni%2bzapiski%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
