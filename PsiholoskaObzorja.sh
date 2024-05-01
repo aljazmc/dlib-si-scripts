44f53667d00193951ce8818fed96f39786c87558
aljazmc <mlinaric.aljaz@gmail.com>
2024-05-01 07:45:36 +0200

    feat: add variables

---
+++ PsiholoskaObzorja.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Psiholoska Obzorja"
+url="https://www.dlib.si/results/?=&query=%27rele%253dPsiholo%25c5%25a1ka%2bobzorja%2b(Ljubljana)%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
