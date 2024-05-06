4b57366ac8a9d0bb9869fbd0c9638939fc12c49c
aljazmc <mlinaric.aljaz@gmail.com>
2024-05-06 07:18:22 +0200

    feat: add directory check

--- PsiholoskaObzorjaClanki.sh
+++ PsiholoskaObzorjaClanki.sh
@@ -3,3 +3,8 @@
 dirname="Psiholoska Obzorja Clanki"
 url="https://www.dlib.si/results/?=&query=%27rele%253dPsiholo%25c5%25a1ka%2bobzorja%2b(Ljubljana)%27&fformattypeserial=article&sortDir=ASC&sort=date&pageSize=100&page="
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
