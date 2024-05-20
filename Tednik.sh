e4739325666c01dcd0717400ac3ae8ede0c46546
aljazmc <mlinaric.aljaz@gmail.com>
2024-05-20 08:17:03 +0200

    feat: add directory check

--- Tednik.sh
+++ Tednik.sh
@@ -3,3 +3,8 @@
 dirname="Tednik"
 url="https://www.dlib.si/results/?=&query=%27rele%253dTednik%2b(Ptuj)%27&sortDir=ASC&sort=date&pageSize=100&page="
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
