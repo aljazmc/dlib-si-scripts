43ae1a97e7585c271e07006544287c17766497e2
aljazmc <mlinaric.aljaz@gmail.com>
2024-05-27 07:04:52 +0200

    feat: add variables

---
+++ StudiaMythologicaSlavica.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dirname="Studia Mythologica Slavica"
+url="https://www.dlib.si/results/?=&query=%27rele%253dStudia%2bmythologica%2bSlavica%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
+
