ba768207c7f6c234295878e6f8aff1e3b9f2c285
aljazmc <mlinaric.aljaz@gmail.com>
2024-05-27 07:41:50 +0200

    feat: add directory check

--- StudiaMythologicaSlavica.sh
+++ StudiaMythologicaSlavica.sh
@@ -3,3 +3,8 @@
 dirname="Studia Mythologica Slavica"
 url="https://www.dlib.si/results/?=&query=%27rele%253dStudia%2bmythologica%2bSlavica%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
