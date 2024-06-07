700c5676484deeead176f8f01a6e7869482d9915
aljazmc <mlinaric.aljaz@gmail.com>
2024-06-07 07:43:57 +0200

    chore: remove directory check

--- ActaAgriculturaeSlovenica.sh
+++ ActaAgriculturaeSlovenica.sh
@@ -3,11 +3,6 @@
 dirname="Acta Agriculturae Slovenica"
 url="https://www.dlib.si/results/?=&query=%27rele%253dActa%2bagriculturae%2bSlovenica%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
-if [ -d "$dirname" ]; then
-  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
-  exit
-fi
-
 mkdir "$dirname"
 cd "$dirname"
 
