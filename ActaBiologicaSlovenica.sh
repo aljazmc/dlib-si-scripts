93a2684c107cc79334cff5ba2abd2b4d28fa0118
aljazmc <mlinaric.aljaz@gmail.com>
2024-06-10 10:04:13 +0200

    chore: add abspath variable

--- ActaBiologicaSlovenica.sh
+++ ActaBiologicaSlovenica.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dirname="Acta Biologica Slovenica"
+abspath="$(cd "$(dirname "$dirname")" && pwd -P)/$(basename "$dirname")/"
 url="https://www.dlib.si/results/?=&query=%27rele%253dActa%2bbiologica%2bslovenica%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
 mkdir "$dirname"
