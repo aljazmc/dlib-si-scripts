9f2adf79e4654cd3359b66dd9453fc06c1376fc1
aljazmc <mlinaric.aljaz@gmail.com>
2024-06-24 19:19:48 +0200

    chore: cleanup

--- AnnalesSeriesHistoriaEtSociologia.sh
+++ AnnalesSeriesHistoriaEtSociologia.sh
@@ -35,4 +35,3 @@ find . -type f -name "PDF*" -print0 | xargs -0I {} sh -c "mv \"{}\" \"$abspath{}
 
 wget -w 2 -i textstodownload && rm TEXT
 find . -type f -name "TEXT*" -print0 | xargs -0I {} sh -c "mv \"{}\" \"$abspath{}\".txt"
-
