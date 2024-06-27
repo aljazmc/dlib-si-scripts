fbffab0bddf98fb905565b43024aebc7af224b25
aljazmc <mlinaric.aljaz@gmail.com>
2024-06-27 08:04:09 +0200

    refactor: add abspath variable

--- AndragoskaSpoznanja.sh
+++ AndragoskaSpoznanja.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dirname="Andragoska Spoznanja"
+abspath="$(cd "$(dirname "$dirname")" && pwd -P)/$(basename "$dirname")/"
 url="https://www.dlib.si/results/?=&query=%27rele%253dAS.%2bAndrago%25c5%25a1ka%2bspoznanja%27&pageSize=100&fformattypeserial=journal&sortDir=ASC&sort=date"
 
 if [ -d "$dirname" ]; then
