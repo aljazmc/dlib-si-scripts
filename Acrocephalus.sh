96c04430c20e9adf38aaea70de3caa0625b9be14
aljazmc <mlinaric.aljaz@gmail.com>
2024-07-02 07:59:53 +0200

    chore: remove directory check

--- Acrocephalus.sh
+++ Acrocephalus.sh
@@ -5,11 +5,6 @@ abspath="$(cd "$(dirname "$dirname")" && pwd -P)/$(basename "$dirname")/"
 tempdir=$(mktemp -d)
 url="https://www.dlib.si/results/?=&query=%27rele%253dAcrocephalus%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100&page="
 
-if [ -d "$dirname" ]; then
-  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
-  exit
-fi
-
 mkdir "$dirname"
 cd "$dirname"
 
