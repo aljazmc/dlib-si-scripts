44b58737af2d3ab1e35c02a855ec9775512a3046
aljazmc <mlinaric.aljaz@gmail.com>
2024-12-30 11:29:26 +0200

    feat: add new downloader

---
+++ DogovoriClanki.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+dirname="Dogovori Clanki"
+url="https://www.dlib.si/results/?=&query=%27rele%253dDogovori%27&pageSize=100&sortDir=ASC&sort=date&page="
+
+if [ -d "$dirname" ]; then
+  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+  exit
+fi
+
+mkdir "$dirname"
+cd "$dirname"
+
+for i in {1..96}
+do
+  wget -w 5 -O "$i" "$url""$i"
+done
+
+cat {1..96} > htmldump
+
+cat htmldump \
+  | sed 's/\/PDF/\/PDF\n/' \
+  | sed 's/\/stream/\n\/stream/g' \
+  | awk 'length == 72' \
+  | sed '/PDF/!d' \
+  | sed 's/^/https\:\/\/www.dlib.si/g' > pdflist
+
+head -1 pdflist > pdfstodownload
+cat pdflist >> pdfstodownload
+
+cat htmldump \
+  | sed 's/\/TEXT/\/TEXT\n/' \
+  | sed 's/\/stream/\n\/stream/g' \
+  | awk 'length == 73' \
+  | sed '/TEXT/!d' \
+  | sed 's/^/https\:\/\/www.dlib.si/g' > textlist
+
+head -1 textlist > textstodownload
+cat textlist >> textstodownload
+
+cat pdfstodownload | xargs wget -w 4
+find . -type f -name "PDF*" -print0 | xargs -0I {} sh -c 'mv "{}" "{}".pdf'
+
+cat textstodownload | xargs wget -w 2
+find . -type f -name "TEXT*" -print0 | xargs -0I {} sh -c 'mv "{}" "{}".txt'
+
+rm *[0-9] htmldump pdflist pdfstodownload PDF.pdf textlist textstodownload TEXT.txt
