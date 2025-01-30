f317cd4baee0eeccaf2f8e67426a0c28839363e7
aljazmc <mlinaric.aljaz@gmail.com>
2025-01-30 10:18:15 +0200

    feat: add new downloader

---
+++ SlovenskiList1896.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+dirname="Slovenski List 1896"
+abspath="$(cd "$(dirname "$dirname")" && pwd -P)/$(basename "$dirname")/"
+tempdir=$(mktemp -d)
+url="https://www.dlib.si/results/?=&query=%27rele%253dSlovenski%2blist%2b(1896)%27&fformattypeserial=journal&sortDir=ASC&sort=date&pageSize=100&page="
+
+cd "$tempdir" || exit
+trap 'rm -rf -- "$tempdir"' EXIT
+
+wget -w 1 "$url"{1..5}
+
+cat index* > htmldump
+
+sed 's/\/PDF/\/PDF\n/' htmldump \
+  | sed 's/\/stream/\n\/stream/g' \
+  | awk 'length == 72' \
+  | sed '/PDF/!d' \
+  | sed 's/^/https\:\/\/www.dlib.si/g' > pdflist
+
+head -1 pdflist > pdfstodownload
+cat pdflist >> pdfstodownload
+
+sed 's/\/TEXT/\/TEXT\n/' htmldump \
+  | sed 's/\/stream/\n\/stream/g' \
+  | awk 'length == 73' \
+  | sed '/TEXT/!d' \
+  | sed 's/^/https\:\/\/www.dlib.si/g' > textlist
+
+head -1 textlist > textstodownload
+cat textlist >> textstodownload
+
+mkdir -p "$abspath"
+
+wget -w 2 -i pdfstodownload && rm PDF
+find . -type f -name "PDF*" -print0 | xargs -0I {} sh -c "mv \"{}\" \"$abspath{}\".pdf"
+
+wget -w 2 -i textstodownload && rm TEXT
+find . -type f -name "TEXT*" -print0 | xargs -0I {} sh -c "mv \"{}\" \"$abspath{}\".txt"
