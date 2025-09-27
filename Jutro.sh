c0cba8e7856aadc828a617bcd45c41e9e790c39e
aljazmc <mlinaric.aljaz@gmail.com>
2025-09-27 09:39:57 +0200

    refactor: use cookies with wget, try to download only missing files, remove wrong downloads and echo the number of missing files

--- Jutro.sh
+++ Jutro.sh
@@ -4,45 +4,128 @@ dirname="Jutro"
 scriptname=$(echo $dirname | tr -d ' ')
 url="https://www.dlib.si/results/?=&query=%27rele%253dJutro%2b(Ljubljana)%27&pageSize=100&fformattypeserial=newspaper&sortDir=ASC&sort=date&page="
 
-if [ -d "$dirname" ]; then
-  echo "Directory \"$dirname\" with downloads already exists. If you want to download periodical again, remove that directory and start the script again."
+if [ ! -f "cookies.txt" ]; then
+  echo "Please extract the 'cookies.txt' from www.dlib.si with the appropriate plugin for your web browser. Save 'cookies.txt' in the same directory as $scriptname.sh"
   exit
 fi
 
-mkdir "$dirname"
-cd "$dirname"
+mkdir -p "$dirname"
+cd "$dirname" || exit
 
 for i in {1..91}
 do
-  wget --max-redirect=3 -w 5 -O "$i" "$url""$i"
+  wget --load-cookies ../cookies.txt -w 7 -O "$i" "$url""$i"
 done
 
 cat {1..91} > htmldump
 
-cat htmldump \
-  | sed 's/\/PDF/\/PDF\n/' \
+HTMLDUMP=$(cat htmldump)
+
+if [[ $HTMLDUMP == *"NAPAKA"* ]]; then
+  echo "" \
+  && echo "There were some problems with downloading files from dlib.si. Please, try again after a few moments." \
+  && rm htmldump \
+  && exit
+fi
+
+################## Extract pdf download links #################################
+
+sed 's/\/PDF/\/PDF\n/' htmldump \
   | sed 's/\/stream/\n\/stream/g' \
   | awk 'length == 72' \
   | sed '/PDF/!d' \
   | sed 's/^/https\:\/\/www.dlib.si/g' > pdflist
 
-head -1 pdflist > pdfstodownload
-cat pdflist >> pdfstodownload
+[ ! -s pdflist ] \
+  && echo "PDF download links weren't extracted successfully. Exiting..." \
+  && exit
 
-cat htmldump \
-  | sed 's/\/TEXT/\/TEXT\n/' \
+################## Extract txt download links #################################
+
+sed 's/\/TEXT/\/TEXT\n/' htmldump \
   | sed 's/\/stream/\n\/stream/g' \
   | awk 'length == 73' \
   | sed '/TEXT/!d' \
   | sed 's/^/https\:\/\/www.dlib.si/g' > textlist
 
-head -1 textlist > textstodownload
-cat textlist >> textstodownload
+[ ! -s textlist ] \
+  && echo "TXT download links weren't extracted successfully. Exiting..." \
+  && exit
 
-cat pdfstodownload | xargs wget --max-redirect=3 -w 4
-find . -type f -name "PDF*" -print0 | xargs -0I {} sh -c 'mv "{}" "{}".pdf'
+#################### create pdf downloader and run it #########################
 
-cat textstodownload | xargs wget --max-redirect=3 -w 2
-find . -type f -name "TEXT*" -print0 | xargs -0I {} sh -c 'mv "{}" "{}".txt'
+PDFSEXPECTED=$(wc -l < pdflist)
 
-rm ./*[0-9] htmldump pdflist pdfstodownload PDF.pdf textlist textstodownload TEXT.txt
+for (( COUNTER=1; COUNTER<="$PDFSEXPECTED"; COUNTER+=1 ));
+do
+  echo "PDF.$COUNTER.pdf" >> pdfsexpected
+done
+
+sort > pdfsexpectedsorted < pdfsexpected
+
+find PDF* -maxdepth 1 -name "PDF*" \
+  | sort > pdfsdownloadedsorted
+
+comm pdfsdownloadedsorted  pdfsexpectedsorted -3 \
+  | sed 's/[PDF\.,\.pdf]//g' \
+  | tr -d '[:blank:]' > pdflinkstodownload
+
+sed -e "s/^/\`sed '/g" \
+  -e "s/$/\!d\' pdflist\`/g" > pdfslist < pdflinkstodownload
+
+sed 's/^/wget -w 5 --load-cookies ..\/cookies.txt -O PDF./g;s/$/.pdf/g' > pdffilenamer < pdflinkstodownload
+
+echo "#!/bin/bash" > pdfrunner
+
+chmod +x pdfrunner
+
+paste -d ' ' pdffilenamer pdfslist >> pdfrunner
+
+./pdfrunner
+
+##################### create txt downloader and run it ########################
+
+TEXTSEXPECTED=$(wc -l < textlist)
+
+for (( COUNTER=1; COUNTER<="$TEXTSEXPECTED"; COUNTER+=1 ));
+do
+  echo "TEXT.$COUNTER.txt" >> textsexpected
+done
+
+sort > textsexpectedsorted < textsexpected
+
+find TEXT* -maxdepth 1 -name "TEXT*" \
+  | sort > textsdownloadedsorted
+
+comm textsdownloadedsorted  textsexpectedsorted -3 \
+  | sed 's/[TEXT\.,\.txt]//g' \
+  | tr -d '[:blank:]' > textlinkstodownload
+
+sed -e "s/^/\`sed '/g" \
+  -e "s/$/\!d\' textlist\`/g" > textslist < textlinkstodownload
+
+sed 's/^/wget -w 5 --load-cookies ..\/cookies.txt -O TEXT./g;s/$/.txt/g' > textfilenamer < textlinkstodownload
+
+echo "#!/bin/bash" > textrunner
+
+chmod +x textrunner
+
+paste -d ' ' textfilenamer textslist >> textrunner
+
+./textrunner
+
+######################## find and remove wrong downloads ######################
+
+find . -type f -exec grep -q 'DOCTYPE html' '{}' \; -delete;
+find . -type f -empty -delete
+
+################################# report ######################################
+
+echo "$(( PDFSEXPECTED-$(find PDF* -maxdepth 1 -name "PDF*"| wc -l) )) pdfs are missing."
+echo "$(( TEXTSEXPECTED-$(find TEXT* -maxdepth 1 -name "TEXT*"| wc -l) )) texts are missing."
+echo "If some files are missing, run the script again"
+
+################################ cleanup ######################################
+
+rm pdf*
+rm text*
